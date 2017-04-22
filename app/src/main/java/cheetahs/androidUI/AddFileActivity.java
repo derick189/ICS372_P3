6a6c7aff3c3104ac3e061469df78aae2ffca6bb7
mcanderson <ao4345zk@metrostate.edu>
2017-04-22 15:16:34 -0500

    Adding error handling for AddFileActivity and a few more code comments

--- app/src/main/java/cheetahs/androidUI/AddFileActivity.java
+++ app/src/main/java/cheetahs/androidUI/AddFileActivity.java
@@ -3,6 +3,7 @@ package cheetahs.androidUI;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
@@ -15,6 +16,10 @@ import java.io.InputStream;
 import cheetahs.controller.Controller;
 import cheetahs.library.Library;
 
+/*
+AddFileActivity runs when the add file button is clicked in MainActivity.
+
+ */
 public class AddFileActivity extends AppCompatActivity implements View.OnClickListener {
     private static final int READ_REQUEST_CODE = 42;
     private Uri uri;
@@ -41,6 +46,7 @@ public class AddFileActivity extends AppCompatActivity implements View.OnClickLi
     }
 
     @Override
+    // Actions for when the Find File and Add File Data buttons are clicked.
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btnFindFile:
@@ -60,8 +66,29 @@ public class AddFileActivity extends AppCompatActivity implements View.OnClickLi
                 }
                 try {
                     inputStream = getContentResolver().openInputStream(uri);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
+                }
+                // Issue with using the file selected - tell user to retry and make sure file is
+                // still available (in case they selected external and it got disconnected, or it
+                // was mis-entered?
+                catch (FileNotFoundException e) {
+                    // Create an alert stating no URI was chosen
+                    AlertDialog.Builder builder = new AlertDialog.Builder(AddFileActivity.this);
+                    // 2. Chain together various setter methods to set the dialog characteristics
+                    builder.setMessage("Could not load that file. Make sure the file is still accessible " +
+                            "in the location that you chose.");
+                    builder.setTitle("Error");
+                    builder.show();
+                    return;
+                }
+                // URI is null if a file isn't picked - catch it and tell the user to select a file
+                catch (NullPointerException e) {
+                    // Create an alert stating no URI was chosen
+                    AlertDialog.Builder builder = new AlertDialog.Builder(AddFileActivity.this);
+                    // 2. Chain together various setter methods to set the dialog characteristics
+                    builder.setMessage("No file was chosen. Select a file first and try again.");
+                    builder.setTitle("Error");
+                    builder.show();
+                    return;
                 }
                 if (uri.toString().endsWith("json")) {
                     fileType = "json";
@@ -81,6 +108,7 @@ public class AddFileActivity extends AppCompatActivity implements View.OnClickLi
     }
 
     @Override
+    // onActivityResult runs when Find File is clicked.
     public void onActivityResult(int requestCode, int resultCode,
                                  Intent resultData) {
         // The ACTION_OPEN_DOCUMENT intent was sent with the request code
